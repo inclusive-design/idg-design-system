7a023bc5fb4ce67ae7642fa36cf78212b2d1ab53
Daniel <chosww@gmail.com>
2023-07-18 14:43:56 -0400

    feat: colors and fonts (#1)

---
+++ src/_includes/components/color/color.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: "Color",
+    context: {
+        colors: [
+            "blue-100",
+            "blue-500",
+            "blue-700",
+            "green-100",
+            "green-300",
+            "green-500",
+            "green-700",
+            "red-100",
+            "red-500",
+            "orange-100",
+            "orange-500"
+        ]
+    }
+};
