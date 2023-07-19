534fb2bc9c6899d6545e7d999e2cfa9f91692fc6
Ned Zimmerman <ned@bight.dev>
2023-07-19 15:41:53 -0300

    feat: add badge component (resolves #11)

---
+++ src/_includes/components/badge/badge.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    title: "Badge",
+    context: {
+        text: "Badge"
+    },
+    variants: [
+        {
+            title: "Insight",
+            context: {
+                variantClass: "badge--insight",
+                text: "Insight"
+            }
+        },
+        {
+            title: "Practice",
+            context: {
+                variantClass: "badge--practice",
+                text: "Practice"
+            }
+        },
+        {
+            title: "Technique",
+            context: {
+                variantClass: "badge--technique",
+                text: "Technique"
+            }
+        },
+        {
+            title: "Activity",
+            context: {
+                variantClass: "badge--activity",
+                text: "Activity"
+            }
+        }
+    ]
+};
