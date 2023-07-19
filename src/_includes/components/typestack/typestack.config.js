33f853a57fe409c3e12f42317f5425c04a10b15f
Daniel Cho <chosww@gmail.com>
2023-07-19 15:19:55 -0400

    fix: rename header component to typestack and replace with existing typestack component

--- src/_includes/components/typestack/typestack.config.js
+++ src/_includes/components/typestack/typestack.config.js
@@ -1,14 +1,51 @@
 module.exports = {
     title: "Typestack",
     context: {
-        examples: {
-            h1: "Inclusive Design Guide",
-            h2: "Inclusive Design Guide",
-            h3_serif: "Recognize diversity and uniqueness",
-            h3_sans: "Recognize diversity and uniqueness",
-            h4: "Insight",
-            body: "It is the responsibility of inclusive designers to be aware of the context and broader impact of any design and strive to effect a beneficial impact beyond the intended beneficiary of the design.",
-            Links: "Previous"
+        text: "Typestack"
+    },
+    variants: [
+        {
+            title: "Header Level 1",
+            context: {
+                level: "h1",
+                text: "Inclusive Design Guide"
+            }
+        },
+        {
+            title: "Header Level 2",
+            context: {
+                level: "h2",
+                text: "Inclusive Design Guide"
+            }
+        },
+        {
+            title: "Header Level 3",
+            context: {
+                level: "h3",
+                text: "Recognize diversity and uniqueness"
+            }
+        },
+        {
+            title: "Header Level 3 (Card)",
+            context: {
+                level: "h3",
+                variantClass: "card",
+                text: "Recognize diversity and uniqueness"
+            }
+        },
+        {
+            title: "Header Level 4",
+            context: {
+                level: "h4",
+                text: "Insight"
+            }
+        },
+        {
+            title: "Paragraph",
+            context: {
+                level: "p",
+                text: "It is the responsibility of inclusive designers to be aware of the context and broader impact of any design and strive to effect a beneficial impact beyond the intended beneficiary of the design."
+            }
         }
-    }
+    ]
 };
