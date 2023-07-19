db7fefaf3781fcd4464efa997d49c488fd9ef58b
Daniel Cho <chosww@gmail.com>
2023-07-19 11:42:46 -0400

    feat: add typestack component

---
+++ src/_includes/components/typestack/typestack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: "Typestack",
+    context: {
+        examples: {
+            h1: "Inclusive Design Guide",
+            h2: "Inclusive Design Guide",
+            h3_serif: "Recognize diversity and uniqueness",
+            h3_sans: "Recognize diversity and uniqueness",
+            h4: "Insight",
+            body: "It is the responsibility of inclusive designers to be aware of the context and broader impact of any design and strive to effect a beneficial impact beyond the intended beneficiary of the design.",
+            Links: "Previous"
+        }
+    }
+};
