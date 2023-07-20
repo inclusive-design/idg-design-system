4bfd9ff4ebf7138aed1cd2d5e59de6e34f9c8a07
Ned Zimmerman <ned@bight.dev>
2023-07-20 18:22:14 -0300

    feat: add flow and prose utilities, use logical properties (#23)

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,9 @@
 const prettify = require("html-prettify");
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(EleventyRenderPlugin);
     eleventyConfig.addPlugin(syntaxHighlight);
 
     eleventyConfig.addPassthroughCopy("./src/static");
