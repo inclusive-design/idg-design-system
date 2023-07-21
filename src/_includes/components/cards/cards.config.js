7a9ba3777a24b88b50019bee33fdbd16884d30d3
Ned Zimmerman <ned@bight.dev>
2023-07-21 14:21:19 -0300

    feat: card components (resolves #15)

---
+++ src/_includes/components/cards/cards.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: "Cards",
+    context: {
+        cards: [
+            {
+                badge: "Insight",
+                variantClass: "insight",
+                title: "Diverse participation and perspectives",
+                intro: "In keeping with the edict “nothing about us without us”, this principle is about inviting a diversity of people with a broad range of needs, preferences, interests and skills."
+            },
+            {
+                badge: "Practice",
+                variantClass: "practice",
+                title: "Diverse participation and perspectives",
+                intro: "In keeping with the edict “nothing about us without us”, this principle is about inviting a diversity of people with a broad range of needs, preferences, interests and skills."
+            },
+            {
+                badge: "Technique",
+                variantClass: "technique",
+                title: "Diverse participation and perspectives",
+                intro: "In keeping with the edict “nothing about us without us”, this principle is about inviting a diversity of people with a broad range of needs, preferences, interests and skills."
+            },
+            {
+                badge: "Activity",
+                variantClass: "activity",
+                title: "Diverse participation and perspectives",
+                intro: "In keeping with the edict “nothing about us without us”, this principle is about inviting a diversity of people with a broad range of needs, preferences, interests and skills."
+            }
+        ]
+    }
+};
