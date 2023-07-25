153215c2a298fe8c8fd3cb2fc7c9d0d7d5ca8e9d
Ned Zimmerman <ned@bight.dev>
2023-07-25 11:20:20 -0300

    feat: mobile menu

--- src/_includes/components/navigation/navigation.config.js
+++ src/_includes/components/navigation/navigation.config.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     variants: [
         {
-            title: "With Current Page Indicator",
+            title: "Current Page - Insights",
             context: {
                 items: [
                     {
@@ -33,6 +33,66 @@ module.exports = {
                     },
                     {
                         title: "Activities"
+                    }
+                ]
+            }
+        },
+        {
+            title: "Current Page - Practices",
+            context: {
+                items: [
+                    {
+                        title: "Insights"
+                    },
+                    {
+                        title: "Practices",
+                        current: true
+                    },
+                    {
+                        title: "Techniques"
+                    },
+                    {
+                        title: "Activities"
+                    }
+                ]
+            }
+        },
+        {
+            title: "Current Page - Techniques",
+            context: {
+                items: [
+                    {
+                        title: "Insights"
+                    },
+                    {
+                        title: "Practices"
+                    },
+                    {
+                        title: "Techniques",
+                        current: true
+                    },
+                    {
+                        title: "Activities"
+                    }
+                ]
+            }
+        },
+        {
+            title: "Current Page - Activities",
+            context: {
+                items: [
+                    {
+                        title: "Insights"
+                    },
+                    {
+                        title: "Practices"
+                    },
+                    {
+                        title: "Techniques"
+                    },
+                    {
+                        title: "Activities",
+                        current: true
                     }
                 ]
             }
