314f1f82dfc4ebd5c2451f33ef2440d503736b95
Daniel <chosww@gmail.com>
2023-07-25 10:28:15 -0400

    feat: pagination link component (resolves #10) (#29)

---
+++ src/_includes/components/pagination/pagination.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    title: "Pagination",
+    context: {
+        text: "Pagination"
+    },
+    variants: [
+        {
+            title: "Insight",
+            context: {
+                variantClass: "pagination--insight"
+            }
+        },
+        {
+            title: "Practice",
+            context: {
+                variantClass: "pagination--practice"
+            }
+        },
+        {
+            title: "Technique",
+            context: {
+                variantClass: "pagination--technique"
+            }
+        },
+        {
+            title: "Activity",
+            context: {
+                variantClass: "pagination--activity"
+            }
+        }
+    ]
+};
