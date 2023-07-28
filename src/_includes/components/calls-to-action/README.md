cc45718b17893bfdd7ef44b35ee1bd4c2c325302
Daniel Cho <chosww@gmail.com>
2023-07-28 13:09:42 -0400

    feat: implement calls to action component

---
+++ src/_includes/components/calls-to-action/README.md
@@ -0,0 +1,1 @@
+# Calls to action
---
+++ src/_includes/components/calls-to-action/calls-to-action.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    title: "Calls To Action",
+    context: {
+        header: "calls to action",
+        body: "calls to action"
+    },
+    variants: [
+        {
+            title: "Insight",
+            context: {
+                header: "Insight",
+                body: "Things we value",
+                variantClass: "insight"
+            }
+        },
+        {
+            title: "Practice",
+            context: {
+                header: "Practice",
+                body: "Things we do throughout the design process",
+                variantClass: "practice"
+            }
+        },
+        {
+            title: "Technique",
+            context: {
+                header: "Technique",
+                body: "Things we do at a specific point in the design process",
+                variantClass: "technique"
+            }
+        },
+        {
+            title: "Activity",
+            context: {
+                header: "Activity",
+                body: "Things we do with our collaborators and co-designers",
+                variantClass: "activity"
+            }
+        }
+    ]
+};
